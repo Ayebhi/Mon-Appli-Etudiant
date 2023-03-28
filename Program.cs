79f7bea8d0b3a004b4a9bd6d97d99ab5bea08a1c
Aissatou28208 <122581840+Aissatou28208@users.noreply.github.com>
2023-03-28 19:12:43 -0400

    creation projet c#

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Application_Etudiant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mon Appli");
+        }
+    }
+}
